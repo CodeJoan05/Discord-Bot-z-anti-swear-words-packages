eeaf3f8f79fdb6b26433060b76ea6fb858be358b
Joanna <93274249+CodeJoan05@users.noreply.github.com>
2024-05-29 19:19:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,14 @@
-const Discord = require('discord.js') //discord.js
-const client = new Discord.Client(); //discord client
-const antiSwearWords = require("anti-swear-words-packages-discord") // module
-const swearwords = require('./swearwords.json')
-const config = require('./config.json')
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const antiSwearWords = require("anti-swear-words-packages-discord");
+const swearwords = require('./swearwords.json');
+const config = require('./config.json');
 
 // Informacje o logowaniu
 
 client.on('ready', () => {
     console.log(`Logged in as ${client.user.tag}!`)
 })
-
-// Module setup
 
 client.on('message', async message => {
 
@@ -19,7 +17,7 @@ client.on('message', async message => {
         ignoreWord: swearwords.json
     });
     
-    message.delete(); // usuwa wiadomość
+    // message.delete();
     message.channel.send(`<@${message.author.id}> użył przekleństwa`)
 });
 
